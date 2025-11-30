c60d9bc14a1490312ffd4ff8b99b7f3812d222c1
shulo <giulibentevenga@gmail.com>
2025-11-30 18:54:30 -0300

    ADD: RestaurantListView

--- backend/restaurant_orders/urls.py
+++ backend/restaurant_orders/urls.py
@@ -1,10 +1,11 @@
 from rest_framework.routers import DefaultRouter
 from django.contrib import admin
 from django.urls import path, include
-from orders.views import OrderViewSet, TerminalQRViewSet, ViewOrderViewSet, AuthViewSet
+from orders.views import OrderViewSet, TerminalQRViewSet, ViewOrderViewSet, AuthViewSet, RestaurantListViewSet
 
 routes = DefaultRouter()  
 
+routes.register(r'restaurants', RestaurantListViewSet, basename='restaurants')
 routes.register(r'orders', OrderViewSet, basename='orders')
 #routes.register(r'terminal_qr', TerminalQRViewSet, basename='terminal_qr')
 routes.register(r'view_order', ViewOrderViewSet, basename='view_order')
