019fac207df83bfd66444e42a8ab35e852527ace
shulo <giulibentevenga@gmail.com>
2025-11-30 18:21:31 -0300

    MOD: urls de vercel

--- backend/orders/views.py
+++ backend/orders/views.py
@@ -13,7 +13,7 @@ from .serializers import RegisterSerializer, LoginSerializer, OrderLookupSeriali
 from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
 from django.contrib.auth import get_user_model
 
-QR_BASE_URL = "https://ding-3ota.vercel.app/r/"
+QR_BASE_URL = "https://ding-3ota.vercel.app/qr/"
 ORDER_BASE_URL = "https://ding-3ota.vercel.app/view_order/"
 
 class OrderViewSet(viewsets.ViewSet):
